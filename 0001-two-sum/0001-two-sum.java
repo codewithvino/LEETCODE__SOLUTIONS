6e153a1fcb6fc6dfa6fd1c76bce38dae8d4faa00
codewithvino <135611271+codewithvino@users.noreply.github.com>
2023-09-26 14:50:31 +0530

    Time: 33 ms (55.07%), Space: 43.8 MB (39.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,13 +2,17 @@ class Solution {
     public int[] twoSum(int[] a, int x) {
 
         int n=a.length;
+        int arr[] = new int[2];
+        arr[0]=0;
+        arr[1]=0;
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
                 if(a[i]+a[j]==x){
-                    return new int[]{i,j};
+                   arr[0]=i;
+                   arr[1]=j;
                 }
             }
         }
-        return new int[]{};
+        return arr;
     }
 }
